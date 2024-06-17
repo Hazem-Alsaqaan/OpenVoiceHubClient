8baaed9498522cd0f8cb771ee8034402a788c455
hazem-alsaqaan <hazem.alsaqaan@gmail.com>
2024-06-17 21:13:41 +0300

    add about page

--- src/pages/about.page/AboutPage.jsx
+++ src/pages/about.page/AboutPage.jsx
@@ -1,13 +1,25 @@
 
+import { useNavigate } from "react-router-dom"
 import SideBar from "../../components/side.bar/SideBar"
 import "./AboutPage.css"
 
 const AboutPage = ()=>{
+    const navigate = useNavigate()
     return(
         <div className="container">
               <SideBar/>
             <div className="tasks-container">
-            <h1>AboutPage</h1>
+            <div className="about-container">
+                <div className="about-center">
+                    <h3>open voice</h3>
+                    <h1>optimizer</h1>
+                    <p>this platform can determine the importance of your tasks. allowing you to be more efficient in your dialy operations</p>
+                    <div className="btn-container">
+                        <button onClick={()=>navigate("/")} className="custom-btn">get started</button>
+                        </div>
+                </div>
+
+            </div>
             </div>
         </div>
     )
